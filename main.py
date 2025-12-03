208dade775d38015221cbc1f36ebac7f0966f184
mm <mmmdev2003@gmail.com>
2025-12-03 15:01:59 +0300

    dev

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ redis_client = redis.Redis(
     host=cfg.monitoring_redis_host,
     port=cfg.monitoring_redis_port,
     password=cfg.monitoring_redis_password,
-    db=5
+    db=4
 )
 key_builder = DefaultKeyBuilder(with_destiny=True)
 storage = RedisStorage(
